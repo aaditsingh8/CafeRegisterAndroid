50f851b0638cccc9cf7dd180d6b05bd0d59bc442
shivan-suratia <shivan.suratia2@gmail.com>
2022-04-25 14:22:42 -0400

    new orders

--- app/src/main/java/com/example/caferegister/YourOrderActivity.java
+++ app/src/main/java/com/example/caferegister/YourOrderActivity.java
@@ -1,12 +1,27 @@
 package com.example.caferegister;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import android.os.Bundle;
 
 public class YourOrderActivity extends AppCompatActivity {
 
     private MainActivity mainActivity;
+
+    private ListView<MenuItem> ordersList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
